8d0640ce82b00e31af70e2c5c6704f3112ec09a7
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-27 12:10:47 -0500

    Initial Commit

---
+++ Shared/RemindersFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersFinalApp.swift
+//  Shared
+//
+//  Created by Vincent Cloutier on 2022-01-27.
+//
+
+import SwiftUI
+
+@main
+struct RemindersFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
